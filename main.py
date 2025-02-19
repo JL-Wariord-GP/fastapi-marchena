18e6f2debf469d218b1e918c7b13197e976077de
Jorge Gomez <gomez.jorge.pa@outlook.com>
2025-02-19 17:24:15 -0500

    Feature: Implementation Cors

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from routes import product
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Ecommerce API", version="1.0")
 
 app.include_router(product.router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ⚠️ En producción, limita esto a los dominios permitidos
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
